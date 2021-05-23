6d3ab25a31558f2a5e2a90b6f5bcd2fb69c59bba
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-05-24 02:50:58 +0530

    ssdf

---
+++ android/app/src/main/kotlin/com/example/mediumlayout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mediumlayout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.teal[300],
+          title: Center(child: Text('Medium Layouts')),
+        ),
+       body: SafeArea(
+         child: Container(
+             child:Row(
+               children: [
+                 Container(height: 30.0,  width: 200.0,  color: Colors.yellow,
+               child:Row(
+                 children: [Icon(Icons.star, size: 30),
+                            Icon(Icons.star, size: 30),
+                            Icon(Icons.star, size: 30),
+                 ],
+               ),
+
+  ),
+           ],
+         ),
+  ),
+  ),
+    ),
+  ),
+  );
+}
+
+
+                 //Container(child: Icon(Icons.star, size: 30)),
+                // Icon(Icons.star, size: 30),
+                // Icon(Icons.star, size: 30),
+
+                 //Container(height: 30.0,  width: 200.0,  color: Colors.yellow,),
+
+
+
+
+
+
+
+
+
+
+
+
+// body: SafeArea(
+         // child: Container(
